1b3134deb8a7e5fc9416e7db757ed2b5568e6ef6
spriglithika <nathaniel.helgesen@liu.se>
2026-02-19 14:12:47 +0100

    Garbage Collected

--- ToTheGrave/old_garbage/ImagNetStuff.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for root, dirs, files in os.walk('/mimer/NOBACKUP/groups/alvis_cvl/datasets/ImageNet_2012'):
-    if root == '/mimer/NOBACKUP/groups/alvis_cvl/datasets/ImageNet_2012/train':
-        print('Train folders: ', len(dirs))
-    if root == '/mimer/NOBACKUP/groups/alvis_cvl/datasets/ImageNet_2012/val':
-        print('Val folders: ', len(dirs))
